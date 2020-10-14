9f2dd10183fb445d001fb339f7e1b0ca87d2dff1
Abdul Haseeb <ahaseeb0104@gmail.com>
2020-10-14 03:54:56 -0400

    Added more frequencies

--- app/Scheduler/Frequencies.php
+++ app/Scheduler/Frequencies.php
@@ -30,7 +30,7 @@ trait Frequencies
 
         $expression = explode(' ', $this->expression);
 
-        array_splice($expression, $position - 1, 1, $value);
+        array_splice($expression, $position - 1, count($value), $value);
 
         $expression = array_slice($expression, 0, 5);
 
@@ -65,5 +65,193 @@ trait Frequencies
     public function everyThirtyMinutes()
     {
         return $this->replaceIntoExpression(1, '*/30');
+    }
+
+    /**
+     * Run the event hourly at the specified minute.
+     *
+     * @param integer $minute
+     * @return \App\Scheduler\Frequencies
+     */
+    public function hourlyAt($minute = 1)
+    {
+        return $this->replaceIntoExpression(1, $minute);
+    }
+
+    /**
+     * Run the event hourly.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function hourly()
+    {
+        return $this->hourlyAt(1);
+    }
+
+    /**
+     * Run the event daily at the specified time.
+     *
+     * @param integer $hour
+     * @param integer $minute
+     * @return \App\Scheduler\Frequencies
+     */
+    public function dailyAt($hour = 0, $minute = 0)
+    {
+        return $this->replaceIntoExpression(1, [$minute, $hour]);
+    }
+
+    /**
+     * Run the event daily.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function daily()
+    {
+        return $this->dailyAt(0, 0);
+    }
+
+    /**
+     * Run the event twice daily at the specified hours.
+     *
+     * @param integer $firstHour
+     * @param integer $lastHour
+     * @return \App\Scheduler\Frequencies
+     */
+    public function twiceDaily($firstHour = 1, $lastHour = 12)
+    {
+        return $this->replaceIntoExpression(1, [0, "{$firstHour},{$lastHour}"]);
+    }
+
+    /**
+     * Run the event on the specified days.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function days()
+    {
+        return $this->replaceIntoExpression(5, implode(',', func_get_args() ?: ['*']));
+    }
+
+    /**
+     * Run the event on Mondays.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function mondays()
+    {
+        return $this->days(1);
+    }
+
+    /**
+     * Run the event on Tuesdays.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function tuesdays()
+    {
+        return $this->days(2);
+    }
+
+    /**
+     * Run the event on Wednesdays.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function wednesdays()
+    {
+        return $this->days(3);
+    }
+
+    /**
+     * Run the event on Thursdays.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function thursdays()
+    {
+        return $this->days(4);
+    }
+
+    /**
+     * Run the event on Fridays.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function fridays()
+    {
+        return $this->days(5);
+    }
+
+    /**
+     * Run the event on Saturdays.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function saturdays()
+    {
+        return $this->days(6);
+    }
+
+    /**
+     * Run the event on Sundays.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function sundays()
+    {
+        return $this->days(7);
+    }
+
+    /**
+     * Run the event on weekdays.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function weekdays()
+    {
+        return $this->days(1, 2, 3, 4, 5);
+    }
+
+    /**
+     * Run the event on weekends.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function weekends()
+    {
+        return $this->days(6, 7);
+    }
+
+    /**
+     * Run the event at the specified time.
+     *
+     * @param integer $hour
+     * @param integer $minute
+     * @return \App\Scheduler\Frequencies
+     */
+    public function at($hour = 0, $minute = 0)
+    {
+        return $this->dailyAt($hour, $minute);
+    }
+
+    /**
+     * Run the event monthly.
+     *
+     * @return \App\Scheduler\Frequencies
+     */
+    public function monthly()
+    {
+        return $this->monthlyOn(1);
+    }
+
+    /**
+     * Run the event monthly on specified day.
+     *
+     * @param integer $day
+     * @return \App\Scheduler\Frequencies
+     */
+    public function monthlyOn($day = 1)
+    {
+        return $this->replaceIntoExpression(1, [0, 0, $day]);
     }
 }
--- tests/Unit/FrequenciesTest.php
+++ tests/Unit/FrequenciesTest.php
@@ -7,7 +7,7 @@ class FrequenciesTest extends TestCase
 {
     public function test_can_set_plain_cron_expression()
     {
-        $frequencies = $this->getMockForTrait(Frequencies::class);
+        $frequencies = $this->frequencies();
         $frequencies->cron('abc');
 
         $this->assertEquals($frequencies->expression, 'abc');
@@ -15,8 +15,7 @@ class FrequenciesTest extends TestCase
 
     public function test_can_replace_into_expression_at_position()
     {
-        $frequencies = $this->getMockForTrait(Frequencies::class);
-        $frequencies->expression = '* * * * *';
+        $frequencies = $this->frequencies();
         $frequencies->replaceIntoExpression(1, 1);
 
         $this->assertEquals($frequencies->expression, '1 * * * *');
@@ -24,17 +23,16 @@ class FrequenciesTest extends TestCase
 
     public function test_can_replace_into_expression_by_chaining()
     {
-        $frequencies = $this->getMockForTrait(Frequencies::class);
-        $frequencies->expression = '* * * * *';
-        $frequencies->replaceIntoExpression(1, 1)->replaceIntoExpression(2, 2);
+        $frequencies = $this->frequencies();
+        $frequencies->replaceIntoExpression(1, 1)
+                    ->replaceIntoExpression(2, 2);
 
         $this->assertEquals($frequencies->expression, '1 2 * * *');
     }
 
     public function test_can_replace_into_expression_with_an_array()
     {
-        $frequencies = $this->getMockForTrait(Frequencies::class);
-        $frequencies->expression = '* * * * *';
+        $frequencies = $this->frequencies();
         $frequencies->replaceIntoExpression(1, [1, 2]);
 
         $this->assertEquals($frequencies->expression, '1 2 * * *');
@@ -42,8 +40,7 @@ class FrequenciesTest extends TestCase
 
     public function test_cannot_replace_into_expression_past_the_end_of_expression()
     {
-        $frequencies = $this->getMockForTrait(Frequencies::class);
-        $frequencies->expression = '* * * * *';
+        $frequencies = $this->frequencies();
         $frequencies->replaceIntoExpression(5, [1, 2]);
 
         $this->assertEquals($frequencies->expression, '* * * * 1');
@@ -51,8 +48,7 @@ class FrequenciesTest extends TestCase
 
     public function test_can_set_every_minute()
     {
-        $frequencies = $this->getMockForTrait(Frequencies::class);
-        $frequencies->expression = '* * * * *';
+        $frequencies = $this->frequencies();
         $frequencies->everyMinute();
 
         $this->assertEquals($frequencies->expression, '* * * * *');
@@ -60,8 +56,7 @@ class FrequenciesTest extends TestCase
 
     public function test_can_set_every_ten_minutes()
     {
-        $frequencies = $this->getMockForTrait(Frequencies::class);
-        $frequencies->expression = '* * * * *';
+        $frequencies = $this->frequencies();
         $frequencies->everyTenMinutes();
 
         $this->assertEquals($frequencies->expression, '*/10 * * * *');
@@ -69,10 +64,185 @@ class FrequenciesTest extends TestCase
 
     public function test_can_set_every_thirty_minutes()
     {
-        $frequencies = $this->getMockForTrait(Frequencies::class);
-        $frequencies->expression = '* * * * *';
+        $frequencies = $this->frequencies();
         $frequencies->everyThirtyMinutes();
 
         $this->assertEquals($frequencies->expression, '*/30 * * * *');
+    }
+
+    public function test_can_set_hourly_at()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->hourlyAt(45);
+
+        $this->assertEquals($frequencies->expression, '45 * * * *');
+    }
+
+    public function test_can_set_hourly()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->hourly();
+
+        $this->assertEquals($frequencies->expression, '1 * * * *');
+    }
+
+    public function test_can_set_daily_at()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->dailyAt(12, 30);
+
+        $this->assertEquals($frequencies->expression, '30 12 * * *');
+    }
+
+    public function test_can_set_daily()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->daily();
+
+        $this->assertEquals($frequencies->expression, '0 0 * * *');
+    }
+
+    public function test_can_set_twice_daily()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->twiceDaily(6, 12);
+
+        $this->assertEquals($frequencies->expression, '0 6,12 * * *');
+    }
+
+    public function test_can_set_twice_daily_using_default_values()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->twiceDaily();
+
+        $this->assertEquals($frequencies->expression, '0 1,12 * * *');
+    }
+
+    public function test_can_set_days()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->days(1, 3, 5);
+
+        $this->assertEquals($frequencies->expression, '* * * * 1,3,5');
+    }
+
+    public function test_can_set_mondays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->mondays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 1');
+    }
+
+    public function test_can_set_tuesdays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->tuesdays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 2');
+    }
+
+    public function test_can_set_wednesdays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->wednesdays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 3');
+    }
+
+    public function test_can_set_thursdays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->thursdays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 4');
+    }
+
+    public function test_can_set_fridays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->fridays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 5');
+    }
+
+    public function test_can_set_saturdays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->saturdays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 6');
+    }
+
+    public function test_can_set_sundays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->sundays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 7');
+    }
+
+    public function test_can_set_weekdays()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->weekdays();
+
+        $this->assertEquals($frequencies->expression, '* * * * 1,2,3,4,5');
+    }
+
+    public function test_can_set_weekends()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->weekends();
+
+        $this->assertEquals($frequencies->expression, '* * * * 6,7');
+    }
+
+    public function test_can_set_at_time()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->at(12, 30);
+
+        $this->assertEquals($frequencies->expression, '30 12 * * *');
+    }
+
+    public function test_can_set_day_and_time()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->at(12, 30)->weekdays();
+
+        $this->assertEquals($frequencies->expression, '30 12 * * 1,2,3,4,5');
+    }
+
+    public function test_can_set_monthly()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->monthly();
+
+        $this->assertEquals($frequencies->expression, '0 0 1 * *');
+    }
+
+    public function test_can_set_monthly_on_specified_day()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->monthlyOn(10);
+
+        $this->assertEquals($frequencies->expression, '0 0 10 * *');
+    }
+
+    public function test_expression_replacement_works_correctly()
+    {
+        $frequencies = $this->frequencies();
+        $frequencies->daily()->daily();
+
+        $this->assertEquals($frequencies->expression, '0 0 * * *');
+    }
+
+    protected function frequencies()
+    {
+        $frequencies = $this->getMockForTrait(Frequencies::class);
+        $frequencies->expression = '* * * * *';
+
+        return $frequencies;
     }
 }
